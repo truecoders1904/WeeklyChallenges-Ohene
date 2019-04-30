7265d50e12eb0cd53feb055ef28212d2331be3eb
Ohene Bonsu <oheneb.bonsu@gmail.com>
2019-04-30 18:36:04 -0500

    Divide

--- ChallengesWithTestsMarkVII/ChallengesSet03.cs
+++ ChallengesWithTestsMarkVII/ChallengesSet03.cs
@@ -40,7 +40,11 @@ namespace ChallengesWithTestsMarkVII
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if (divisor == 0)
+            {
+                return 0;
+            }
+            return dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
